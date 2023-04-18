08dfad805a5e02067006a1b516dbb8dc6d19f8f1
LuiiVasquez <luivasque95@gmail.com>
2023-04-17 19:34:37 -0600

    fixed urls in api menu

--- server/api/routes/index.ts
+++ server/api/routes/index.ts
@@ -5,8 +5,8 @@ const router = Router();
 
 router.get("/", ({ headers: { host } }: Request, res: Response) => {
   const menu: { [key: string]: string } = {
-    registrations: `http://${host}/api/v1/registrations`,
-    users: `http://${host}/api/v1/users`,
+    registrations: `https://${host}/api/v1/registrations`,
+    users: `https://${host}/api/v1/users`,
   };
 
   response.success(res, 200, "api menu", menu);
