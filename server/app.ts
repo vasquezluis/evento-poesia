c5795c1da3fb7f4bce54eb84871cd6184a298a08
vasquezluis <luivasquez95@gmail.com>
2024-08-18 16:38:54 -0600

    fix: change origin to cors for render deploy

--- server/app.ts
+++ server/app.ts
@@ -9,7 +9,12 @@ import authRoutes from "./api/routes/auth.routes";
 
 const app = express();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({
+    origin: "https://poesia-ticket.netlify.app/",
+    credentials: true,
+  })
+);
 app.use(morgan("dev"));
 
 app.use(indexRoutes);
